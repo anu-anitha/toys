709304d8bf7aff5b957e857a2a09c5df9564c33a
anu-antiha <gadhamanitha@gmail.com>
2022-02-16 19:10:55 +0530

    commit 2

--- toypr/toypr/settings.py
+++ toypr/toypr/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=(tj-$a0q6nm&=at+4_4u51%s$-y1bp)iemi99=!1=_bd*9pea'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['toyss-app.herokuapp.com']
 
 
 # Application definition
