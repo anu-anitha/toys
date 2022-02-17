bd5694bc90108322e3f63e0dc8346091a07d35c8
anu-antiha <gadhamanitha@gmail.com>
2022-02-17 12:18:00 +0530

    commit 4

--- toypr/toypr/settings.py
+++ toypr/toypr/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['toyss-app.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -43,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
